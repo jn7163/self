7990d3e6da472416fe0e0b898c8f6d62efd4d92f
max2max <2206460474@qq.com>
2017-09-03 03:45:38 -0500

    Update new-centos.sh

--- new-centos.sh
+++ new-centos.sh
@@ -34,4 +34,12 @@ make -j4 && make install && cd /root/mpc-1.0.3
 ./configure --prefix=/opt/mpc-1.0.3 --with-gmp=/opt/gmp-6.1.0 --with-mpfr=/opt/mpfr-3.1.4
 make -j4 && make install && cd /root/isl-0.16.1
 ./configure --prefix=/opt/isl-0.16.1 --with-gmp-prefix=/opt/gmp-6.1.0
-make -j4 && make install
+make -j4 && make install && cd
+export LD_LIBRARY_PATH=/opt/mpc-1.0.3/lib:/opt/gmp-6.1.0/lib:/opt/mpfr-3.1.4/lib:/opt/isl-0.16.1/lib
+rm -rf gcc-7.2.0/test
+mkdir gcc-7.2.0/test && cd gcc-7.2.0/test
+../configure --prefix=/opt/gcc-7.2.0 -enable-threads=posix -enable-languages=c,c++ --with-gmp=/opt/gmp-6.1.0 --with-mpfr=/opt/mpfr-3.1.4 --with-mpc=/opt/mpc-1.0.3 --with-isl=/opt/isl-0.16.1 -disable-multilib
+make -j4 > make.log 2>makeerror.log
+make install && cd
+echo 'export PATH="$PATH:/opt/gcc-7.2.0/bin"' >> /etc/profile
+echo 'export LD_LIBRARY_PATH="/opt/gcc-7.2.0/lib64:/opt/mpc-1.0.3/lib:/opt/gmp-6.1.0/lib:/opt/mpfr-3.1.4/lib:/opt/isl-0.16.1/lib"' >> /etc/profile
