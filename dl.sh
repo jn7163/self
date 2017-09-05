a493e81feef33a8f8c8ceb3919b6e82f56cf37b3
max2max <2206460474@qq.com>
2017-09-05 22:28:12 +0800

    Create dl.sh

---
+++ dl.sh
@@ -0,0 +1,5 @@
+
+wget https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
+wget https://dl.google.com/android/repository/android-ndk-r14b-linux-x86_64.zip
+unzip sdk-tools-linux-3859397.zip
+unzip android-ndk-r14b-linux-x86_64.zip
