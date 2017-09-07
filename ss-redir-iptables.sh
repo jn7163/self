768c3d58f569fff799b3974182a6de42f195be23
max2max <2206460474@qq.com>
2017-09-07 08:56:29 +0800

    Update ss-redir-iptables.sh

--- ss-redir-iptables.sh
+++ ss-redir-iptables.sh
@@ -1,4 +1,6 @@
+## single ip
 iptables -t nat -N SHADOWSOCKS
-iptables -t nat -A SHADOWSOCKS -d 123.123.123.123 -j RETURN
-iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports 1081
-iptables -t nat -A PREROUTING -p tcp -j SHADOWSOCKS
+iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-port 1081
+iptables -t nat -A PREROUTING -p tcp -d 123.123.123.123 -j SHADOWSOCKS
+service iptables save
+service iptables restart
