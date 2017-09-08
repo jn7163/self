b249c015f097fb52a29115f41ca42f9d4c4681b5
max2max <2206460474@qq.com>
2017-09-08 20:17:25 +0800

    Update ssr-make.sh

--- ssr-make.sh
+++ ssr-make.sh
@@ -1,43 +1,47 @@
-yum install epel-release -y
-yum update -y
-yum upgrade -y
-yum install -y pam-devel.i686 openssl openssl-devel lzo lzo-devel pam pam-devel automake pkgconfig python-setuptools vim git wget m2crypto
-yum install -y apr* autoconf automake bison bzip2 bzip2* cloog-ppl compat* cpp curl curl-devel fontconfig fontconfig-devel freetype freetype* freetype-devel gcc gcc-c++ gtk+-devel gd gettext gettext-devel glibc kernel kernel-headers keyutils keyutils-libs-devel krb5-devel libcom_err-devel libpng libpng-devel libjpeg* libsepol-devel libselinux-devel libstdc++-devel libtool* libgomp libxml2 libxml2-devel libXpm* libtiff libtiff* make mpfr ncurses* ntp openssl openssl-devel patch pcre-devel perl php-common php-gd policycoreutils telnet t1lib t1lib* nasm nasm* wget zlib-devel swig python-devel libmcrypt-devel readline-devel
-yum -y groupinstall "Development Tools"
-
 curl https://bintray.com/sbt/rpm/rpm | tee /etc/yum.repos.d/bintray-sbt-rpm.repo
 yum install sbt -y
+yum install java-1.8.0-openjdk java-1.8.0-openjdk-devel -y
 
 wget https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
-# wget https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
 wget https://dl.google.com/android/repository/android-ndk-r14b-linux-x86_64.zip
-# wget https://dl.google.com/android/repository/android-ndk-r13-linux-x86_64.zip
 unzip sdk-tools-linux-3859397.zip
-# tar zxvf android-sdk_r24.4.1-linux.tgz
+mkdir /root/android-sdk && mv /root/tools /root/android-sdk
 unzip android-ndk-r14b-linux-x86_64.zip
-# unzip android-ndk-r13-linux-x86_64.zip
 
-echo 'export ANDROID_HOME="/root/tools"' >> /etc/profile
+echo 'export ANDROID_HOME="/root/android-sdk"' >> /etc/profile
 echo 'export ANDROID_NDK_HOME="/root/android-ndk-r14b"' >> /etc/profile
-echo 'export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_NDK_HOME"' >> /etc/profile
+echo 'export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/26.0.1:$ANDROID_NDK_HOME"' >> /etc/profile
 source /etc/profile
+
 echo "y" | android update sdk --filter tools,platform-tools,build-tools-23.0.2,android-23,extra-google-m2repository --no-ui -a
+mkdir /root/.android && touch /root/.android/repositories.cfg
+/root/android-sdk/tools/bin/sdkmanager "platform;"
+/root/android-sdk/tools/bin/sdkmanager "buildtools;26.0.1"
+/root/android-sdk/tools/bin/sdkmanager "extra;google;m2repository"
+/root/android-sdk/tools/bin/sdkmanager "extra;android;m2repository"
 
-vim /etc/profile
-export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/23.0.2:$ANDROID_NDK_HOME"
-source /etc/profile
+git clone https://github.com/shadowsocksr-backup/shadowsocksr-android
 
-echo "y" | android update sdk --filter extra-android-m2repository --no-ui --no-https -a
-android update sdk --no-ui
-
-git clone https://github.com/glzjin/shadowsocksr-android
-
-## 向vps上传一个jks密钥文件，同时编辑密钥设置
+keytool -genkeypair -alias testname -keystore /root/ssr.keystore -sigalg MD5withRSA -keyalg RSA
 
 cd shadowsocksr-android
 cp local.properties.example local.properties
 vim local.properties
 
+echo "rm -rf /lib64/libc.so.6
+LD_PRELOAD=/lib64/libc-2.12.so ln -s /lib64/libc-2.12.so /lib64/libc.so.6" >> /root/default-glibc.sh
+chmod +x /root/default-glibc.sh
+echo "ver=2.14
+rm -rf /lib64/libc.so.6
+LD_PRELOAD=/opt/glibc-${ver}/lib/libc-${ver}.so ln -s /opt/glibc-${ver}/lib/libc-${ver}.so /lib64/libc.so.6" >> /root/glibc-2.14.sh
+chmod +x /root/glibc-2.14.sh
+echo "ver=2.15
+rm -rf /lib64/libc.so.6
+LD_PRELOAD=/opt/glibc-${ver}/lib/libc-${ver}.so ln -s /opt/glibc-${ver}/lib/libc-${ver}.so /lib64/libc.so.6" >> /root/glibc-2.15.sh
+chmod +x /root/glibc-2.15.sh
+
 git submodule update --init
+/root/glibc-2.15.sh
 ./build.sh
+/root/glibc-2.14.sh
 sbt clean android:package-release
