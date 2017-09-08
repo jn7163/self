27e1649ccda581bda3dbc1977bf0843c5c9dbf2d
max2max <2206460474@qq.com>
2017-09-08 20:48:26 +0800

    Delete dl.sh

--- dl.sh
+++
@@ -1,5 +0,0 @@
-
-wget https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
-wget https://dl.google.com/android/repository/android-ndk-r14b-linux-x86_64.zip
-unzip sdk-tools-linux-3859397.zip
-unzip android-ndk-r14b-linux-x86_64.zip
