66ac06a9bd5033c15828b6a39b09c4b46e371443
max2max <2206460474@qq.com>
2017-09-07 20:56:29 -0500

    Update keytool.sh

--- keytool.sh
+++ keytool.sh
@@ -1,3 +1,3 @@
-keytool -genkey -alias testname -keystore /root/ssr.keystore -sigalg MD5withRSA -keyalg RSA ##ssr编译用时，最后一步密码留空即可
+keytool -genkeypair -alias testname -keystore /root/ssr.keystore -sigalg MD5withRSA -keyalg RSA ##ssr编译用时，最后一步密码留空即可
 keytool -list -keystore /root/ssr.keystore
 keytool -delete -keystore /root/ssr.keystore
