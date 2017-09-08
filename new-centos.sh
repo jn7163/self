5d370bab268f8fa553f427fcb168e514f82cfcf7
max2max <2206460474@qq.com>
2017-09-08 20:57:25 +0800

    Update new-centos.sh

--- new-centos.sh
+++ new-centos.sh
@@ -41,6 +41,3 @@ mkdir gcc-7.2.0/test && cd gcc-7.2.0/test
 ../configure --prefix=/opt/gcc-7.2.0 -enable-threads=posix -enable-languages=c,c++ --with-gmp=/opt/gmp-6.1.0 --with-mpfr=/opt/mpfr-3.1.4 --with-mpc=/opt/mpc-1.0.3 --with-isl=/opt/isl-0.16.1 -disable-multilib
 make -j4 > make.log 2>makeerror.log
 make install && cd
-echo 'export PATH="$PATH:/opt/gcc-7.2.0/bin"' >> /etc/profile
-echo 'export LD_LIBRARY_PATH="/opt/gcc-7.2.0/lib64:/opt/mpc-1.0.3/lib:/opt/gmp-6.1.0/lib:/opt/mpfr-3.1.4/lib:/opt/isl-0.16.1/lib"' >> /etc/profile
-echo 'export CC=/opt/gcc-7.1.0/bin/gcc' >> /etc/profile
