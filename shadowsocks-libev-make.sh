f737f3f11fb32521a405fca877750304dbf7174f
max2max <2206460474@qq.com>
2017-09-08 21:12:13 +0800

    Create shadowsocks-libev-make.sh

---
+++ shadowsocks-libev-make.sh
@@ -0,0 +1,25 @@
+## need high level gcc & export CC=...
+git clone https://github.com/shadowsocks/shadowsocks-libev.git
+cd shadowsocks-libev
+git submodule update --init --recursive
+cd
+yum install gettext make asciidoc xmlto c-ares-devel libev-devel -y
+export LIBSODIUM_VER=1.0.13
+wget https://dl.ccavs.org/libsodium-$LIBSODIUM_VER.tar.gz
+tar xvf libsodium-$LIBSODIUM_VER.tar.gz
+pushd libsodium-$LIBSODIUM_VER
+./configure --prefix=/usr && make
+sudo make install
+popd
+ldconfig
+export MBEDTLS_VER=2.6.0
+wget https://dl.ccavs.org/mbedtls-$MBEDTLS_VER-gpl.tgz
+tar xvf mbedtls-$MBEDTLS_VER-gpl.tgz
+pushd mbedtls-$MBEDTLS_VER
+make SHARED=1 CFLAGS=-fPIC
+sudo make DESTDIR=/usr install
+popd
+ldconfig
+cd shadowsocks-libev
+./autogen.sh && ./configure --prefix=/opt/shadowsocks-libev && make
+make install
