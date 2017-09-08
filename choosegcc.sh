5e3eee4336941ef71512d77cab57ca177475fa91
max2max <2206460474@qq.com>
2017-09-08 21:00:35 +0800

    Update and rename choosegcc.sh to gcc-choose.sh

--- choosegcc.sh
+++
@@ -1,7 +0,0 @@
-## default gcc-4.4
-yum install gcc -y
-
-## delete default gcc
-yum remove gcc cpp -y
-
-## others
---
+++ gcc-choose.sh
@@ -0,0 +1,18 @@
+## default gcc-4.4
+yum install gcc -y
+
+## delete default gcc
+yum remove gcc cpp -y
+
+## others
+
+export mygccver="7.1.0"
+export mygmpver="6.1.0"
+export mympcver="1.0.3"
+export mympfrver="3.1.4"
+export myislver="0.16.1"
+export LD_LIBRARY_PATH="/opt/gcc-${mygccver}/lib:/opt/gcc-${mygccver}/lib64:/opt/gmp-${mygmpver}/lib:/opt/mpc-${mympcver}/lib:/opt/mpfr-${mympfrver}/lib:/opt/isl-${myislver}/lib"
+export PATH="$PATH:/opt/gcc-${mygccver}/bin"
+export CC=/opt/gcc-${mygccver}/bin/gcc
+export CXX=/opt/gcc-${mygccver}/bin/g++
+export CPP=/opt/gcc-${mygccver}/bin/cpp
