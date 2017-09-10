4e929aa5def56e67cc438b467ab546d808f6ff2a
max2max <2206460474@qq.com>
2017-09-09 23:23:16 -0500

    Update shadowsocks-libev-make.sh

--- shadowsocks-libev-make.sh
+++ shadowsocks-libev-make.sh
@@ -8,8 +8,8 @@ export LIBSODIUM_VER=1.0.13
 wget https://dl.ccavs.org/libsodium-$LIBSODIUM_VER.tar.gz
 tar xvf libsodium-$LIBSODIUM_VER.tar.gz
 pushd libsodium-$LIBSODIUM_VER
-./configure --prefix=/usr && make
-sudo make install
+./configure --prefix=/usr && make -j4
+make install
 popd
 ldconfig
 export MBEDTLS_VER=2.6.0
@@ -17,14 +17,15 @@ wget https://dl.ccavs.org/mbedtls-$MBEDTLS_VER-gpl.tgz
 tar xvf mbedtls-$MBEDTLS_VER-gpl.tgz
 pushd mbedtls-$MBEDTLS_VER
 make SHARED=1 CFLAGS=-fPIC
-sudo make DESTDIR=/usr install
+make DESTDIR=/usr install
 popd
 ldconfig
 cd shadowsocks-libev
-./autogen.sh && ./configure --prefix=/opt/shadowsocks-libev && make
+./autogen.sh && ./configure --prefix=/opt/shadowsocks-libev && make -j4
 make install && cd
 cd /etc/init.d
 wget https://raw.githubusercontent.com/max2max/self/master/ssredir
 chkconfig ssredir on
 chmod +x ssredir
 cd /etc/ && wget https://raw.githubusercontent.com/max2max/self/master/ss.json -O ss-redir.json
+vim ss-redir.json
